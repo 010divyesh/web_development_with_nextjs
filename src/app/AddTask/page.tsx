1e7f2e53575de3e4e334ebc2a44abf0521127760
010divyesh <divyesh1998chaudhari@gmail.com>
2024-02-13 17:13:52 +0530

    Spinner_add

--- src/app/AddTask/page.tsx
+++ src/app/AddTask/page.tsx
@@ -4,22 +4,26 @@ import Nav from "@/component/Navbar/index";
 import Form from "@/component/TaskForm";
 import { taskDef } from "@/component/types";
 import { saveTasksToLocal, getTaskFromLocal } from "@/component";
+import axios from "axios";
+import Spinner from "@/component/Spinner";
 
 export default function AddTask() {
   
-  const save = (task: taskDef) => {
-    let tasks = [];
-    if (localStorage.tasks) {
-      tasks = getTaskFromLocal();
-    }
-    tasks.push(task);
-    saveTasksToLocal(tasks);
+  const [loading, setLoading] = useState(false);
+
+  const save = async(task: taskDef) => {
+    setLoading(true)
+      const res = await axios.post(
+        "https://crudcrud.com/api/8864861aca83475a82865ac299a7c9a1/task",task
+      );
+      setLoading(false)
   };
 
   return (
     <main>
       <Nav title="Add Task" backbtn={true} />
       <Form submitBtnLable="ADD" onSave={save}/>
+      {loading && <Spinner/>}
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,50 +6,60 @@ import "./page.module.css";
 import Nav from "@/component/Navbar";
 import { taskDef } from "@/component/types";
 import { saveTasksToLocal, getTaskFromLocal } from "@/component";
+import axios from "axios";
+import Spinner from "@/component/Spinner";
 
 
 
 export default function Home() {
   const [tasks, setTasks] = useState<taskDef[]>([])
+  const [loading, setLoading] = useState(false);
 
   const deleteTask = (i:number)=>{
-  if(tasks[i].scheduled) return alert("You cannot delete this task");
-  let tasks2 = Object.assign([], tasks);
-  tasks2.splice(i,1);
-  setTasks(tasks2);
-  saveTasksToLocal(tasks2);
+  // if(tasks[i].scheduled) return alert("You cannot delete this task");
+  // let tasks2 = Object.assign([], tasks);
+  // tasks2.splice(i,1);
+  // setTasks(tasks2);
+  // saveTasksToLocal(tasks2);
   }
 
   const scheduledTask = (i: number) => {
-    let tasks2: taskDef[] = Object.assign([], tasks);
-    tasks2[i].scheduled = !tasks2[i].scheduled;
-    setTasks(tasks2);
-    saveTasksToLocal(tasks2);
+    // let tasks2: taskDef[] = Object.assign([], tasks);
+    // tasks2[i].scheduled = !tasks2[i].scheduled;
+    // setTasks(tasks2);
+    // saveTasksToLocal(tasks2);
   };
 
   useEffect(()=>{
-    if(localStorage.tasks){
-      setTasks(getTaskFromLocal())
-    }
+   (async () => {
+      setLoading(true)
+      const res = await axios.get(
+        "https://crudcrud.com/api/8864861aca83475a82865ac299a7c9a1/task"
+      );
+      setTasks(res.data);
+      setLoading(false)
+      // console.log(res.data)
+    })();
   },[])
 
   return (
     <main>
       <Nav title="TODO List" rightIcon="calender" />
-      <div className="todo-center-container" >
+      {loading && <Spinner/>}
+      { !loading && <div className="todo-center-container" >
         <div className="todo-list-container" id="todo-list"></div>
-        {tasks.map((task,i)=>(<div className="todo-bar" key={task.title}>
+        {tasks.map((task)=>(<div className="todo-bar" key={task.title}>
              <div className="todo-bar-left-section">
                <div className="title">{task.title}</div>
                <div className="sub-title">{task.detail}</div>
              </div>
              <div className="todo-bar-right-section">
-                <Link href={`/EditTask/${i}`} ><img src="./icons/pen.svg" /></Link>
-               <img src="./icons/delet.svg" onClick={()=>deleteTask(i)}  />
-               <img src="./icons/checkcircle.svg"   onClick={()=>scheduledTask(i)} className={task.scheduled ? "scheduled" : ""}/>
+                <Link href={`/EditTask/${task._id}`} ><img src="./icons/pen.svg" /></Link>
+               <img src="./icons/delet.svg" onClick={()=>deleteTask(task._id)}  />
+               <img src="./icons/checkcircle.svg"   onClick={()=>scheduledTask(task._id)} className={task.scheduled ? "scheduled" : ""}/>
              </div>
            </div>))}
-           </div>
+           </div>}
            <div>
         <div className="add-todo-button">
           {/* <Link href="/AddTask" className="button">+</Link> */}
